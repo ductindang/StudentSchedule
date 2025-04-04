7c0c513258082619db8eec56a8b2e06471e67fe9
Đặng Đức Tin <100546291+ductindang@users.noreply.github.com>
2025-04-04 23:19:25 +0700

    Delete Project directory

--- Project/StudentManageFrontEnd/StudentManageFrontEnd/Models/Subject.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StudentManageFrontEnd.Models
-{
-    public class Subject
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
